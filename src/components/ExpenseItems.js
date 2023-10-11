488f9a4f170c3447a9adbc4addf4d0df8d01f057
ravikeshri2910 <ravikeshri2910@gmail.com>
2023-10-11 17:27:49 +0530

    Lets start coding

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,17 @@
+function ExpenseItem(){
+
+    return (
+    <div>
+
+        <h3>ExpenseItem!</h3>
+        <p>Food Rs 10</p>
+        <p>Petrol Rs 100</p>
+        <p>Movies Rs 200</p>
+    </div>
+    )
+
+
+
+}
+
+export default ExpenseItem
